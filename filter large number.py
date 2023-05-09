79fa13340cc3ca73e107c9e616fe10cf14b42d81
Rafiul islam <128176232+WD-Rafiul@users.noreply.github.com>
2023-05-09 23:30:16 +0600

    Add files via upload

---
+++ filter large number.py
@@ -0,0 +1,18 @@
+d = {"ineuron" : {"a" : 14, "b" : 10, "c" : 4}, "course" : {"d" : 50, "e" : 34, "f" : 1}, "g" : 5+4j,
+     "h" : [45,6,7,8,9,3], "i" : (45,25,2), "k" : "sudh", "l" : 110}
+d1 = []
+for i in d.values():
+    if type(i) == dict or type(i) == list or type(i) == tuple:
+        if type(i) == list or type(i)== tuple:
+            for a in i:
+                d1.append(a)
+        else:
+            for a in i.values():
+                d1.append(a)
+    elif type(i) == str or type(i) == complex:
+        pass
+    else:
+        d1.append(i)
+d1.sort()
+
+print(d1[-1])
